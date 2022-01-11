e1c497ef6c1b3c5a65c43110e423f12f09a87216
LukDrey <Lukas.Dreyling@gmx.net>
2022-01-11 09:52:50 +0100

    Data Upload

---
+++ beech_micro_communities.R
@@ -0,0 +1,7 @@
+
+#########################################################################
+##  Drivers of diversity and community structure of micro-communities  ##
+##         (fungi, algae, bacteria) on the bark of beech trees         ##
+#########################################################################
+
+# This is the code accompanying the 
